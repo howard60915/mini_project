7717dc9f95f3680454f6b8c118ae3ead51b349fa
陳璟皓 <howard60915@gmail.com>
2016-10-21 11:11:12 +0800

    PaperClip default picture update for blank pic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,6 @@ class Comment < ApplicationRecord
 	belongs_to :user
 	belongs_to :topic ,:counter_cache => true
 
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/blank.png"
   	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
