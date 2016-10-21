bb439d32de2d9498dba4a91ea97d1a9a78454c91
陳璟皓 <howard60915@gmail.com>
2016-10-21 12:50:29 +0800

    Paperclip default picture update

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -12,7 +12,7 @@ class Topic < ApplicationRecord
 	delegate :email, :to => :user , :prefix => true, :allow_nil => true
 	delegate :content, :to => :comment , :prefix => true, :allow_nil => true
 
-	has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/blank.png"
   	validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
 
 
