5ce6173ecb2627cd6b9e1fc4ffe3aac36286b6a2
陳璟皓 <howard60915@gmail.com>
2016-10-28 16:02:36 +0800

    Heroku bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :topics do 
-  	resources :comments, :controller => :comments, only: [:new, :create, :edit, :update, :destroy]
+  	resources :comments,  only: [:new, :create, :edit, :update, :destroy]
     resources :subscribes, :controller => :subscribes, only: [:create, :destroy]  
     # /topics/:topic_id/subscribes
     
